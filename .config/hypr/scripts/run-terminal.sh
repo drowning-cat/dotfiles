d8e6f4ab4f4207da945247153af1bc763a7c0e53
Drowning Cat <shamal.ma.personal@gmail.com>
2025-11-20 21:18:49 +0300

    M  .config/hypr/scripts/run-terminal.sh

--- .config/hypr/scripts/run-terminal.sh
+++ .config/hypr/scripts/run-terminal.sh
@@ -2,5 +2,5 @@
 
 session_id=$(tmux display -p '#{next_session_id}' | cut -c2-)
 session_id=${session_id:-0}
-alacritty -e tmux new-session -s "session_${session_id}__autoremove" $1
+alacritty -e tmux new-session -s "session_${session_id}__autoremove" ${1:+"$1"}
 
